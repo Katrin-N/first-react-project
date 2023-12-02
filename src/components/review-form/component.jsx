a416a9c274bbf323a19787c0166539ab435396c1
Нанивская Екатерина <nanivskaya.e@wildberries.work>
2023-12-02 21:23:43 +0300

    style[menu]: Стилизация формы

--- src/components/review-form/component.jsx
+++ src/components/review-form/component.jsx
@@ -23,36 +23,38 @@ const reducer = (state, action) => {
     }
 };
 
+import styles from "./styles.module.css";
 
 export const ReviewForm = () => {
     const [formValue, dispatch] = useReducer(reducer, DEFAULT_FORM_VALUE);
 
     console.log(formValue);
-    return <div>
-       <h2>Оставьте отзыв о ресторане</h2>
-        <div>
-            <div>
-                <label htmlFor="name">Имя</label>
-                <input
-                    id="name"
-                    type="text"
-                    value={formValue.name}
-                    onChange={(event) =>
-                        dispatch({ type: "setName", payload: event.target.value })
-                    }
-                />
-            </div>
-            <div>
-                <label htmlFor="name">Текст</label>
-                <input
-                    id="name"
-                    type="text"
-                    value={formValue.text}
-                    onChange={(event) =>
-                        dispatch({ type: "setText", payload: event.target.value })
-                    }
-                />
-            </div>
+    return <div className={styles.decor}>
+        <div className={styles.formLeftDecoration}></div>
+        <div className={styles.formRightDecoration}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.formInner}>
+            <h2 className={styles.title}>Оставьте отзыв о ресторане</h2>
+            <input
+                className={styles.input}
+                id="name"
+                type="text"
+                placeholder="Имя"
+                value={formValue.name}
+                onChange={(event) =>
+                    dispatch({ type: "setName", payload: event.target.value })
+                }
+            />
+            <input
+                id="name"
+                type="text"
+                className={styles.input}
+                placeholder="Текст"
+                value={formValue.text}
+                onChange={(event) =>
+                    dispatch({ type: "setText", payload: event.target.value })
+                }
+            />
             <div>
                 <label htmlFor="name">Рейтинг</label>
                 <Counter minValue={0} maxValue={5} step={0.5} value={formValue.rating} onChangeCounter={(value) => dispatch({type:"setRating", payload: value})} />
